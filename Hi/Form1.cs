fe80f86a70a4d66177842aae405028ceecb25bc4
nnlinh070602 <nnl070602@gmail.com>
2022-10-16 14:18:54 +0700

    Xoa

--- Hi/Form1.cs
+++ Hi/Form1.cs
@@ -16,5 +16,32 @@ namespace Hi
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(445, 427);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+
+        }
     }
 }
